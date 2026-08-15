330803b6098589cdf917f7e7a9534d7b4ab30ce8
Momen <mouminmouhamed28@gmail.com>
2026-08-15 09:25:46 +0300

    Import path module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const todoRoutes = require("./todo-api/routes/todoRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
